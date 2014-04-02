97b0d755eda9d6e1ccf784f6eacf83ef608e37c5
naphatkrit <naphat.krit@gmail.com>
2014-04-02 14:26:13 -0400

    UI: save page (agenda vs calendar) when closing. There is a bug in selecting the initial page

--- nice/static/js/main.js
+++ nice/static/js/main.js
@@ -1,8 +1,30 @@
 $(init)
+var NAV_ID = ["agendatab", "calendartab"];
+var TAB_ID = ["agenda", "calendar"];
 
 function init()
 {
+    Nav_load();
     PopUp_init();
     Agenda_init();
     Cal_init();
+    $(window).bind("beforeunload", function(){
+        Nav_save();
+    });
+}
+function Nav_save()
+{
+    var id = $("#maintab").find(".active").find("a")[0].id;
+    $.cookie("nav_page", NAV_ID.indexOf(id));
+}
+function Nav_load()
+{
+    var index = $.cookie("nav_page");
+    if (index == null)
+        return;
+    $("#maintab #"+id).tab("show");
+    //$("#maintab li").removeClass("active");
+    //$("#maintab #"+NAV_ID[index]).parent().addClass("active");
+    //$(".tab-pane").removeClass("in");
+    //$("#"+TAB_ID[index]).addClass("in");
 }
