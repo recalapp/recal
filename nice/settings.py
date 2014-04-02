2dbc789cf0ca82f1b5e12fb514e37de0a82bfe11
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2014-04-02 14:38:33 -0400

    Configuring sqlite for now, mysql might be overkill

--- nice/settings.py
+++ nice/settings.py
@@ -80,8 +80,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
