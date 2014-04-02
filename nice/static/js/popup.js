8fb58f8ca527620c63bdee798e1f220f134dac79
naphatkrit <naphat.krit@gmail.com>
2014-04-02 17:33:06 -0400

    UI: added one style of edit page

--- nice/static/js/popup.js
+++ nice/static/js/popup.js
@@ -57,7 +57,7 @@ function PopUp_setFirstDrag(popUp, firstDrag)
 
 function PopUp_insertPopUp(isMain)
 {
-    $("body").append('<div id="popup-main123" class="popup-container popup"><div class="panel panel-default panel-clipped"><div class="panel-heading panel-heading-handle"><h3 class="panel-title"><span class="popup-title">title</span><a href="#" onclick="PopUp_clickedClose(this); return false;" class="popup-ctrl hide"><span class="glyphicon glyphicon-remove"></span></a></h3></div><div class="panel-body panel-body-scroll"><h4 id="popup-date" onclick="PopUp_clickedElement(this)">April 11, 2014 at 11:00AM</h4><input id="popup-date-form" type="text" class="form-control hide"></input><h4 id="popup-loc">CS Building</h4><p id="popup-type">Assignment</p><p id="popup-desc">The content of the selected agenda will go here. This popup by default will change according to which agenda is selected. However, if the user drags this popup, it will stay forever.<br>The content of the selected agenda will go here. This popup by default will change according to which agenda is selected. However, if the user drags this popup, it will stay forever.</p></div></div></div>');
+    $("body").append('<div id="popup-main123" class="popup-container popup"><div class="panel panel-default panel-clipped"><div class="panel-heading panel-heading-handle"><h3 class="panel-title"><span class="popup-title">title</span><a href="#" onclick="PopUp_clickedClose(this); return false;" class="popup-ctrl hide"><span class="glyphicon glyphicon-remove"></span></a></h3></div><div class="panel-body panel-body-scroll"><h4 id="popup-date" onclick="PopUp_clickedElement(this)">April 11, 2014 at 11:00AM</h4><div class="input-group hide" id="popup-date-form"><input type="text" class="form-control"><span class="input-group-btn"><button class="btn btn-success" type="button" onclick="PopUp_clickedSaveElement($(this).parent().parent()[0])"><span class="glyphicon glyphicon-ok"></span></button><button class="btn btn-danger" type="button" onclick="PopUp_clickedCancelElement($(this).parent().parent()[0])"><span class="glyphicon glyphicon-remove" ></span></button></span></div><h4 id="popup-loc">CS Building</h4><p id="popup-type">Assignment</p><p id="popup-desc">The content of the selected agenda will go here. This popup by default will change according to which agenda is selected. However, if the user drags this popup, it will stay forever. <br>The content of the selected agenda will go here. This popup by default will change according to which agenda is selected. However, if the user drags this popup, it will stay forever.</p></div></div></div>');
     var popUp = $("#popup-main123");
     popUp.draggable({handle:'.panel > .panel-heading', containment:"#content_bounds", scroll: false}).find(".panel").resizable({
         stop: function(e, ui){
@@ -200,4 +200,49 @@ function PopUp_load()
 function PopUp_hasFocus(popUp)
 {
     return $(popUp).find(".panel").hasClass("panel-primary");
+}
+function PopUp_showFormForElement(element)
+{
+    var popUp = _PopUp_getPopUp(element);
+    $(element).addClass("hide");
+    var form_id = element.id + "-form";
+    var form = $(popUp).find("#" + form_id).removeClass("hide")[0];
+    return form_id;
+}
+function PopUp_hideFormForElement(form)
+{
+    var popUp = _PopUp_getPopUp(form);
+    $(form).addClass("hide");
+    var text_id = form.id.split("-").slice(0, -1).join("-");
+    $(popUp).find("#"+text_id).removeClass("hide");
+    return text_id;
+}
+function PopUp_clickedElement(element)
+{
+    var popUp = _PopUp_getPopUp(element);
+    // make the corresponding form visible and hide the element
+    var form_id = PopUp_showFormForElement(element);
+    var form = $(popUp).find("#"+form_id);
+    $(form).find("input")[0].value = element.innerHTML;
+    $(form).find("input")[0].focus();
+}
+function PopUp_clickedSaveElement(form)
+{
+    var popUp = _PopUp_getPopUp(form);
+    // hide the form and unhide the text
+    var text_id = PopUp_hideFormForElement(form);
+    $(popUp).find("#"+text_id)[0].innerHTML = $(form).find("input")[0].value;
+}
+function PopUp_clickedCancelElement(form)
+{
+    var popUp = _PopUp_getPopUp(form);
+    // hide the form and unhide the text
+    PopUp_hideFormForElement(form);
+}
+function _PopUp_getPopUp(child)
+{
+    var popUp = child;
+    while (!$(popUp).hasClass("popup"))
+        popUp = $(popUp).parent()[0];
+    return popUp;
 }
