c55af035caf12225f7384415283722bf7c5f9a45
Dyland Xue <dylandxue2012@gmail.com>
2014-04-03 10:05:37 -0400

    fixed no reverse match bug when running locally. Now using explicit setup for django_debug_toolbar

--- nice/urls.py
+++ nice/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -15,3 +15,9 @@ urlpatterns = patterns('',
     url(r'popup-template', views.popup, name="popup"),
     url(r'^testview', views.testview, name="testview"),
 )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
