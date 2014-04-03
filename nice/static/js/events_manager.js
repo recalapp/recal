b444cf8722ad9e6499625f465c59372e9a3f9895
naphatkrit <naphat.krit@gmail.com>
2014-04-03 12:08:43 -0400

    UI: added skeleton code for events manager

--- nice/static/js/events_manager.js
+++ nice/static/js/events_manager.js
@@ -1,11 +1,76 @@
-eventsManager = null;
-
+var eventsManager = null;
+var EVENTS_INIT = false;
 
 function EventsMan_init()
 {
+    if (EVENTS_INIT)
+        return;
+    EVENTS_INIT = true;
+    eventsManager = new _EventsMan_new();
+    // TODO should populate events manager
 }
 
 function _EventsMan_new()
 {
-    return {events: {}};
+    this.events = {};
+    this.order = []; // {start: "start", id: "id"}, keep sorted
+    return this;
 }
+
+/**************************************************
+ * Client methods
+ * These are strictly interactions between the event
+ * manager and the client. It does NOT talk to 
+ * the server.
+ **************************************************/
+
+//{
+//    id: "id",
+//    title: "title",
+//    type: "type",
+//    description: "description",
+//    start: "start unix time",
+//    end: "end unix time",
+//    loc: "location",
+//    recurring: // how??
+//}
+function EventsMan_getEventByID(id)
+{
+    return this.events[id];
+}
+
+function EventsMan_getEventIDForRange(start, end)
+{
+    var index = 0;
+    while (eventsManager.order[i].start < start)
+        i++;
+    var iStart = i;
+    while (eventsManager.order[i].start <= end)
+        i++;
+    var iEnd = ++i; // slice method is exclusive on the right end
+    var ret = eventsManager.order.slice(iStart, iEnd);
+    for (var i = 0; i < ret.length; i++)
+        ret[i] = ret[i].id;
+    return ret;
+}
+function EventsMan_updateEventForID(id, eventDict)
+{
+    // TODO should verify eventDict
+    eventsManager.events[id] = eventDict;
+    // TODO contact server
+}
+function EventsMan_addEventForID(id, eventDict)
+{
+    // TODO should verify eventDict
+    eventsManager.events[id] = eventDict;
+    // TODO contact server
+}
+function EventsMan_deleteEvent(id)
+{
+    delete eventsManager.events[id];
+    // TODO contact server
+}
+
+/***************************************************
+ * Server code
+ **************************************************/
