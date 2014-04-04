e04daf47d41c59427d4d10e0da97d9091d1bf775
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2014-04-03 22:15:38 -0400

    Setting up userprofile class in admin (the models are coming in a commit shortly)

--- nice/admin.py
+++ nice/admin.py
@@ -4,3 +4,20 @@ from django.contrib import admin
 from nice.models import *
 
 admin.site.register(Event)
+
+from nice.models import UserProfile
+
+# Define an inline admin descriptor for UserProfile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'student'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
