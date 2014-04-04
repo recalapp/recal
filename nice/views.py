b36405123ad47baf1bca7a0c841c5e62c976ea18
Dyland Xue <dylandxue2012@gmail.com>
2014-04-03 22:53:16 -0400

    added cas module

--- nice/views.py
+++ nice/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import * # render, redirect
-from django.http import * # HttpResponse
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from nice.models import *
 
 # Views go here.
 
 def index(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('cas_login'))
     return render(request, 'main/index.html', None)
 
 def popup(request):
