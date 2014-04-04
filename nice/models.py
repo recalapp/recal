0fd514ebd78f41b8e72760c8f16b620187664d43
naphatkrit <naphat.krit@gmail.com>
2014-04-03 22:11:29 -0400

    MODELS: modified models.py

--- nice/models.py
+++ nice/models.py
@@ -5,13 +5,17 @@ from django.db import models
 # Run python manage.py sql nice to see SQL for our models.
 # To apply a model: python manage.py syncdb again. (This changes in later versions of Django, beware!)
 
+# Relationships note: 
+# use foreign key for many to one rel
+# use OneToOneField for one to one rel
+
 class Section(models.Model):
     pass
 
 class Event_Group(models.Model):
     # relationships
     section = models.ForeignKey(Section)
-    best_revision = models.ForeignKey(Event_Group_Revision, rel_class=models.OneToOneRel)
+    best_revision = models.OneToOneField('Event_Group_Revision')
     
 class Event_Group_Revision(models.Model):
     # relationships
@@ -20,12 +24,13 @@ class Event_Group_Revision(models.Model):
     # fields
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    recurrence_days = models.
     # TODO add recurrence field
     
 class Event(models.Model):
     # relationships
     group = models.ForeignKey(Event_Group)
-    best_revision = models.ForeignKey(Event_Revision, rel_class=models.OneToOneRel)
+    #best_revision = models.ForeignKey(Event_Revision, rel_class=models.OneToOneRel)
     
 class Event_Revision(models.Model):
     TYPE_ASSIGNMENT = "AS"
@@ -56,5 +61,4 @@ class Event_Revision(models.Model):
     event_location = models.CharField(max_length=100)
     # TODO modified_user = 
     modified_time = models.DateTimeField()
-
-
+    approved = models.BooleanField(default=True) # TODO change default value
