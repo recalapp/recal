4ce23a79cab9a643313297eddbe49752292f69fe
naphatkrit <naphat.krit@gmail.com>
2014-04-07 14:30:00 -0400

    QUERIES: added code for outputing in json at /netid. no support for start and end date yet

--- nice/views.py
+++ nice/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from nice.models import *
-from nice.queries import *
+from nice import queries
+
+import json
 
 # Views go here.
 
@@ -13,12 +15,16 @@ def index(request):
         return HttpResponseRedirect(reverse('cas_login'))
     return render(request, 'main/index.html', None)
 
+# loading templates
+
 def popup(request):
     return render(request, 'main/popup.html', None)
 def agenda(request):
     return render(request, 'main/agenda.html', None)
 def typepicker(request):
     return render(request, 'main/type-picker.html', None)
-	
-def testview(request):
-    return HttpResponse("Hello")
+
+# for AJAX
+def events_json(request, netid):
+    events = queries.get_events(netid)
+    return HttpResponse(json.dumps(events), mimetype='application/javascript')
