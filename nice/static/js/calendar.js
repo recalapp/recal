6c4b9ec33a0c33a1a03e903dbc198c864e6e6f84
naphatkrit <naphat.krit@gmail.com>
2014-04-08 19:54:53 -0400

    FRONT-END: finished calendar code

--- nice/static/js/calendar.js
+++ nice/static/js/calendar.js
@@ -1,13 +1,31 @@
 CAL_INIT = false;
+Cal_eventSource = {
+    events:[],
+}
 
 $(document).ready(function() {
     //Cal_init();
 });
 
+//eventSources: [{
+//    events: [{
+//            id: "1",
+//            title: "item 1",
+//            start: 1396114200,
+//            end: 1396117200
+//        }]
+//}],
+
+
 function Cal_init() {
     if (CAL_INIT)
         return;
     height = window.innerHeight - $(".navbar").height() - 50;
+
+    EventsMan_addUpdateListener(function(){
+        Cal_reload();
+    });
+
     $("#calendarui").fullCalendar({
         "defaultView": "agendaWeek",
         "slotMinutes": 30,
@@ -20,71 +38,7 @@ function Cal_init() {
         eventBackgroundColor: "#74a2ca",
         eventBorderColor: "#428bca",
         allDayDefault: false,
-        eventSources: [{
-            events: [
-                {
-                    id: "1",
-                    title: "item 1",
-                    start: 1396114200,
-                    end: 1396117200
-                }, {
-                    id: "2",
-                    title: "item 2",
-                    start: 1396009800,
-                    end: 1396012800
-                }, {
-                    id: "3",
-                    title: "item 3",
-                    start: 1396018800,
-                    end: 1396023600
-                }, {
-                    id: "4",
-                    title: "item 4",
-                    start: 1396027800,
-                    end: 1396032600
-                }, {
-                    id: "5",
-                    title: "item 5",
-                    start: 1395928800,
-                    end: 1395931800
-                }, {
-                    id: "6",
-                    title: "item 6",
-                    start: 1395941400,
-                    end: 1395944400
-                }, {
-                    id: "7",
-                    title: "item 7",
-                    start: 1395860400,
-                    end: 1395865200
-                }, {
-                    id: "8",
-                    title: "item 8",
-                    start: 1395759600,
-                    end: 1395764400
-                }, {
-                    id: "9",
-                    title: "item 9",
-                    start: 1395765000,
-                    end: 1395768000
-                }, {
-                    id: "10",
-                    title: "item 10",
-                    start: 1395768600,
-                    end: 1395773400
-                }, {
-                    id: "11",
-                    title: "item 11",
-                    start: 1395669600,
-                    end: 1395674400
-                }, {
-                    id: "12",
-                    title: "item 12",
-                    start: 1395673200,
-                    end: 1395678000
-                }
-            ]
-        }],
+        eventSources: [Cal_eventSource],
         eventClick: function(calEvent, jsEvent, view) {
             if (calEvent.highlighted == true)
             {
@@ -95,26 +49,33 @@ function Cal_init() {
             if (SHIFT_PRESSED)
             {
                 Cal_highlightEvent(calEvent, true);
-                calEvent.pinned = true;
+                UI_pin(calEvant.id);
+                //calEvent.pinned = true;
                 var popUp = PopUp_insertPopUp(null, false);
-                PopUp_setID(popUp, calEvent.id);
-                PopUp_setTitle(popUp, calEvent.title);
+                PopUp_setToEventID(popUp, calEvent.id);
+                //PopUp_setID(popUp, calEvent.id);
+                //PopUp_setTitle(popUp, calEvent.title);
                 PopUp_giveFocus(popUp);
                 return;
             }
 
             $($("#calendarui").fullCalendar("clientEvents", function(calEvent) {
-                return calEvent.pinned != true;
+                return !UI_isPinned(calEvent.id)
             })).each( function(index) {
                 Cal_unhighlightEvent(this, false);
             });
             Cal_highlightEvent(calEvent, true);
 
             var popUp = PopUp_getMainPopUp(function() {
-                calEvent.pinned = true;
+                                //calEvent.pinned = true;
             });
-            PopUp_setID(popUp, calEvent.id);
-            PopUp_setTitle(popUp, calEvent.title);
+            PopUp_setFirstDrag(popUp, function() {
+                UI_pin(calEvent.id);
+            })
+
+            PopUp_setToEventID(popUp, calEvent.id);
+            //PopUp_setID(popUp, calEvent.id);
+            //PopUp_setTitle(popUp, calEvent.title);
             PopUp_giveFocus(popUp);
         }
     });
@@ -123,7 +84,8 @@ function Cal_init() {
         if (this.id == "calendar")
         {
             $(this).bind("webkitTransitionEnd transitionend otransitionend oTransitionEnd", function(e) {
-                Cal_syncFromPopUp();
+                Cal_reload();
+                //Cal_syncFromPopUp();
             });
         }
     });
@@ -132,14 +94,40 @@ function Cal_init() {
     });
     PopUp_addCloseListener(function(id){
         $($("#calendarui").fullCalendar("clientEvents", id)).each(function (index){
-            this.pinned = false;
+            UI_unpin(this.id)
+            //this.pinned = false;
             Cal_unhighlightEvent(this, true);
         });
     });
 }
+function Cal_reload()
+{
+    //return;
+    var endDate = moment().add('months', 3).unix();
+    var startDate = moment().subtract('months', 3).unix();
+    var eventIDs = EventsMan_getEventIDForRange(startDate, endDate);
+    Cal_eventSource.events = [];
+    $.each(eventIDs, function(index){
+        eventDict = EventsMan_getEventByID(this);
+        Cal_eventSource.events.push({
+            id: eventDict.event_id,
+            title: eventDict.event_title,
+            start: eventDict.event_start,
+            end: eventDict.event_end
+        });
+    });
+    $("#calendarui").fullCalendar("refetchEvents");
+    $($('#calendarui').fullCalendar('clientEvents')).each(function(index){
+        if (UI_isPinned(this.id) || UI_isMain(this.id))
+            Cal_highlightEvent(this, false)
+    });
+    Cal_render();
+
+}
 
 function Cal_syncFromPopUp()
 {
+    return;
     if (!EventsMan_ready())
         return;
     $($("#calendarui").fullCalendar("clientEvents")).each(function(index){
