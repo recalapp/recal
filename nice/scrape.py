74d90777298f1da3419870a82b6816cfff1161c3
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2014-04-12 13:58:24 -0400

    created scraper script file

---
+++ nice/scrape.py
@@ -0,0 +1,18 @@
+"""
+Scrapes OIT's Web Feeds to add courses and sections to database.
+
+Procedure:
+
+- Get list of departments (3-letter department codes)
+- Run this: http://etcweb.princeton.edu/webfeeds/courseofferings/?term=current&subject=COS
+- Parse it for courses, sections, and lecture times (as recurring events)
+
+TODO(Dyland): write this!
+
+"""
+from nice.models import *
+from nice import queries
+import json
+
+
+
