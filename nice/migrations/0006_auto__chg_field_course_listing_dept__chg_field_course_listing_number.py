05f2be914db219658de422872995235655249a30
Dyland Xue <dylandxue2012@gmail.com>
2014-04-12 21:58:16 -0400

    fixed course and course_listing models, added scrape.py for initial data

---
+++ nice/migrations/0006_auto__chg_field_course_listing_dept__chg_field_course_listing_number.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Course_Listing.dept'
+        db.alter_column(u'nice_course_listing', 'dept', self.gf('django.db.models.fields.CharField')(max_length=10))
+
+        # Changing field 'Course_Listing.number'
+        db.alter_column(u'nice_course_listing', 'number', self.gf('django.db.models.fields.CharField')(max_length=10))
+
+    def backwards(self, orm):
+
+        # Changing field 'Course_Listing.dept'
+        db.alter_column(u'nice_course_listing', 'dept', self.gf('django.db.models.fields.CharField')(max_length=3))
+
+        # Changing field 'Course_Listing.number'
+        db.alter_column(u'nice_course_listing', 'number', self.gf('django.db.models.fields.CharField')(max_length=3))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'nice.course': {
+            'Meta': {'object_name': 'Course'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'professor': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'semester': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Semester']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'nice.course_listing': {
+            'Meta': {'object_name': 'Course_Listing'},
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Course']"}),
+            'dept': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        u'nice.event': {
+            'Meta': {'object_name': 'Event'},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Event_Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'nice.event_group': {
+            'Meta': {'object_name': 'Event_Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Section']"})
+        },
+        u'nice.event_group_revision': {
+            'Meta': {'object_name': 'Event_Group_Revision'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            'event_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Event_Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'modified_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.User_Profile']"}),
+            'recurrence_days': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'recurrence_interval': ('django.db.models.fields.IntegerField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {})
+        },
+        u'nice.event_revision': {
+            'Meta': {'object_name': 'Event_Revision'},
+            'approved': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Event']"}),
+            'event_description': ('django.db.models.fields.TextField', [], {}),
+            'event_end': ('django.db.models.fields.DateTimeField', [], {}),
+            'event_location': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'event_start': ('django.db.models.fields.DateTimeField', [], {}),
+            'event_title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'event_type': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'modified_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.User_Profile']"})
+        },
+        u'nice.section': {
+            'Meta': {'object_name': 'Section'},
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Course']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'isDefault': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'all_students'", 'max_length': '100'})
+        },
+        u'nice.semester': {
+            'Meta': {'object_name': 'Semester'},
+            'end_date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {}),
+            'term_code': ('django.db.models.fields.CharField', [], {'default': "'1144'", 'max_length': '4'})
+        },
+        u'nice.user_profile': {
+            'Meta': {'object_name': 'User_Profile'},
+            'hidden_events': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lastActivityTime': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'sections': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['nice.Section']", 'null': 'True', 'through': u"orm['nice.User_Section_Table']", 'blank': 'True'}),
+            'ui_state_restoration': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'profile'", 'unique': 'True', 'to': u"orm['auth.User']"})
+        },
+        u'nice.user_section_table': {
+            'Meta': {'object_name': 'User_Section_Table'},
+            'add_date': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.Section']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['nice.User_Profile']"})
+        }
+    }
+
+    complete_apps = ['nice']
--- nice/scrape.py
+++ nice/scrape.py
@@ -6,45 +6,46 @@ Procedure:
 - Get list of departments (3-letter department codes)
 - Run this: http://etcweb.princeton.edu/webfeeds/courseofferings/?term=current&subject=COS
 - Parse it for courses, sections, and lecture times (as recurring events)
-
-TODO(Dyland): write this!
-
 """
-# from nice.models import *
-# from nice import queries
-from models import *
-import queries
 
-from xml.dom import minidom
+from nice.models import *
 from lxml import etree
 import string
 import sys
 import urllib2
 from bs4 import BeautifulSoup
 import re
-import json
 
 TERM_CODE = 1144  # spring 2014
 COURSE_OFFERINGS = "http://registrar.princeton.edu/course-offerings/"
 FEED_PREFIX = "http://etcweb.princeton.edu/webfeeds/courseofferings/"
 TERM_PREFIX = FEED_PREFIX + "?term=" + str(TERM_CODE)
 DEP_PREFIX = TERM_PREFIX + "&subject="
+PTON_NAMESPACE = u'http://as.oit.princeton.edu/xml/courseofferings-1_3'
 
-CURRENT_SEMESTER
+CURRENT_SEMESTER = ''
 
 def get_current_semester():
     global CURRENT_SEMESTER
     if not CURRENT_SEMESTER:
         try:
-            CURRENT_SEMESTER = Semester.object.get()
-        parser = etree.XMLParser(ns_clean=True)
-        termxml = urllib2.urlopen(TERM_PREFIX)
-        tree = etree.parse(termxml, parser)
-        remove_namespace(dep_courses, u'http://as.oit.princeton.edu/xml/courseofferings-1_3')
-        term = tree.getroot().find('term')
-        start_date = term.find('start_date')
-        end_date = term.find('end_date')
-
+            CURRENT_SEMESTER = Semester.object.get(term_code=str(TERM_CODE))
+        except:
+            parser = etree.XMLParser(ns_clean=True)
+            termxml = urllib2.urlopen(TERM_PREFIX)
+            tree = etree.parse(termxml, parser)
+            remove_namespace(tree, PTON_NAMESPACE)
+            term = tree.getroot().find('term')
+            start_date = term.find('start_date').text
+            end_date = term.find('end_date').text
+            curr_sem = Semester(
+                start_date = start_date, 
+                end_date = end_date, 
+                term_code = str(TERM_CODE)
+            )
+            curr_sem.save()
+            CURRENT_SEMESTER = curr_sem
+    return CURRENT_SEMESTER
 
 # Seed page should be
 # "http://registrar.princeton.edu/course-offerings/"
@@ -68,17 +69,48 @@ def scrape(department):
     tree = etree.parse(xmldoc, parser)
     dep_courses = tree.getroot()
     """ for now hardwire the namespaces--too annoying"""
-    remove_namespace(dep_courses, u'http://as.oit.princeton.edu/xml/courseofferings-1_3')
+    remove_namespace(dep_courses, PTON_NAMESPACE)
     for term in dep_courses:
         for subjects in term:
             for subject in subjects:
                 for courses in subject:
                     for course in courses:
-                        parse_course(course)
+                        parse_course(course, subject)
 
-def parse_course(course):
+## Parse it for courses, sections, and lecture times (as recurring events)
+def parse_course(course, subject):
     """ create a course with the basic information. """
-    pass
+    title = course.find('title').text
+    description = course.find('detail').find('description').text
+    if not description:
+        description = ''
+
+    sub = subject.find('code').text
+    catalog = course.find('catalog_number').text
+    course_listings = [(sub, catalog)]
+    if course.find('crosslistings') is not None:
+        for cross_listing in course.find('crosslistings'):
+            course_listings.append((cross_listing.find('subject').text, cross_listing.find('catalog_number').text))
+
+    new_course = Course(
+        semester=get_current_semester(), 
+        title=title, 
+        description=description,
+        professor=''
+    )
+
+    new_course.save()
+    for course_listing in course_listings:
+        new_listing = Course_Listing(
+            course=new_course,
+            dept=course_listing[0],
+            number=course_listing[1]
+        )
+        new_listing.save()
+        
+    #print " ".join([str(listing) for listing in course_listings])
+    #print title.encode('utf-8'), description.encode('utf-8')
+    #print
 
 def remove_namespace(doc, namespace):
     """Hack to remove namespace in the document in place."""
@@ -88,129 +120,6 @@ def remove_namespace(doc, namespace):
         if elem.tag.startswith(ns):
             elem.tag = elem.tag[nsl:]
 
-scrape_all()
+# scrape_all()
 
 ###########################################################################################
-# def clean(str):
-#     "Return a string with leading and trailing whitespace gone and all other whitespace condensed to a single space."
-#     return re.sub('\s+', ' ', str.strip())
-# 
-# def get_course_details(soup):
-#   "Returns a dict of {courseid, area, title, descrip, prereqs}."
-#   area = clean(soup('strong')[1].findAllNext(text=True)[1])  # balanced on a pinhead
-#   if re.match(r'^\((LA|SA|HA|EM|EC|QR|STN|STL)\)$', area):
-#     area = area[1:-1]
-#   else:
-#     area = ''
-# 
-#   match = re.match(r'\(([A-Z]+)\)', clean(soup('strong')[1].findNext(text=True)))
-#   pretitle = soup.find(text="Prerequisites and Restrictions:")
-#   descrdiv = soup.find('div', id='descr')
-#   return {
-#     'courseid': COURSE_URL_REGEX.search(soup.find('a', href=COURSE_URL_REGEX)['href']).group('id'),
-#     'area': area, #bwk: this was wrong[1:-1],    # trim parens #  match.group(1) if match != None else ''
-#     'title': clean(soup('h2')[1].string),
-#     ###'descrip': clean(descrdiv.contents[0] if descrdiv else ''),
-#     'descrip': clean(flatten(descrdiv)),
-#     'prereqs': clean(pretitle.parent.findNextSibling(text=True)) if pretitle != None else ''
-#   }
-# 
-# def flatten(dd):
-#   s = ""
-#   try:
-#     for i in dd.contents:
-#       try:
-#         s += i
-#       except:
-#         s += flatten(i)
-#   except:
-#     s += "oh, dear"
-#   return s
-# 
-# def get_course_profs(soup):
-#   "Return a list of {uid, name} dicts for the professors teaching this course."
-#   prof_links = soup('a', href=PROF_URL_REGEX)
-#   return [{'uid': PROF_URL_REGEX.search(link['href']).group('id'), 'name': clean(link.string)} for link in prof_links]
-# 
-# def get_single_class(row):
-#   "Helper function to turn table rows into class tuples."
-#   cells = row('td')
-#   time = cells[2].string.split("-")
-#   bldg_link = cells[4].strong.a
-# 
-#   # <td><strong>Enrolled:</strong>0
-#   # <strong> Limit:</strong>11</td>
-#   enroll = ''
-#   limit = ''
-#   if cells[5] != None:    # bwk
-#     enroll = cells[5].strong.nextSibling.string.strip()
-#     limit = cells[5].strong.nextSibling.nextSibling.nextSibling.string.strip()
-# 
-#   return {
-#     'classnum': cells[0].strong.string,
-#     'section': cells[1].strong.string,
-#     'days': re.sub(r'\s+', '', cells[3].strong.string),
-#     'starttime': time[0].strip(),
-#     'endtime': time[1].strip(),
-#     'bldg': bldg_link.string.strip(),
-#     'roomnum': bldg_link.nextSibling.string.replace('&nbsp;', ' ').strip(),
-#     'enroll': enroll, # bwk
-#     'limit': limit   #bwk
-#   }
-# 
-# def get_course_classes(soup):
-#   "Return a list of {classnum, days, starttime, endtime, bldg, roomnum} dicts for classes in this course."
-#   class_rows = soup('tr')[1:] # the first row is actually just column headings
-#   # This next bit tends to cause problems because the registrar includes precepts and canceled
-#   # classes. Having text in both 1st and 4th columns (class number and day of the week)
-#   # currently indicates a valid class.
-#   return [get_single_class(row) for row in class_rows if row('td')[0].strong and row('td')[3].strong.string]
-# 
-# def scrape_page(page):
-#   "Returns a dict containing as much course info as possible from the HTML contained in page."
-#   soup = BeautifulSoup(page).find('div', id='contentcontainer')
-#   course = get_course_details(soup)
-#   course['listings'] = get_course_listings(soup)
-#   course['profs'] = get_course_profs(soup)
-#   course['classes'] = get_course_classes(soup)
-#   return course
-# 
-# def scrape_id(id):
-#   page = urllib2.urlopen(COURSE_URL.format(term=TERM_CODE, courseid=id))
-#   return scrape_page(page)
-# 
-# def scrape_all():
-#   """
-#   Return an iterator over all courses listed on the registrar's site.
-#   
-#   Which courses are retrieved are governed by the globals at the top of this module,
-#   most importantly LIST_URL and TERM_CODE.
-# 
-#   To be robust in case the registrar breaks a small subset of courses, we trap
-#   all exceptions and log them to stdout so that the rest of the program can continue.
-#   """
-#   search_page = urllib2.urlopen(LIST_URL.format(term=TERM_CODE))
-#   courseids = get_course_list(search_page)
-# 
-#   n = 0
-#   for id in courseids:
-#     try:
-#       if n > 99999:
-#         return
-#       n += 1
-#       yield scrape_id(id)
-#     except Exception:
-#       import traceback
-#       traceback.print_exc(file=sys.stderr)
-#       sys.stderr.write('Error processing course id {0}\n'.format(id))
-# 
-# if __name__ == "__main__":
-#   first = True
-#   for course in scrape_all():
-#     if first:
-#       first = False
-#       print '['
-#     else:
-#       print ','
-#     json.dump(course, sys.stdout)
-#   print ']'
