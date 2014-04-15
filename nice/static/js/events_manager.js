f24d5ac57cee0860d312473edcfe27fe3554c1ad
naphatkrit <naphat.krit@gmail.com>
2014-04-15 14:04:00 -0400

    FRONT-END: made it so that if you don't save modification, when you reopen the popup, it shows save button

--- nice/static/js/events_manager.js
+++ nice/static/js/events_manager.js
@@ -191,7 +191,7 @@ function EventsMan_addEvent()
         event_type: 'AS',
         event_start: moment().unix(),
         event_end: moment().minute(moment().minute() + 50).unix(),
-        event_description: 'Event description',
+        event_description: 'Event description \n Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         event_location: 'Event location',
         section_id: SP_firstSectionKey(), 
         modified_user: USER_NETID,
