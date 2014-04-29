f7137650cd1b5424c7d46c2f22b47fdc039af041
Dyland Xue <dylandxue2012@gmail.com>
2014-04-29 10:35:35 -0400

    fixed prettified admin site. Requires pip install -r requirements.txt

--- nice/urls.py
+++ nice/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^login/$', cas.views.login, name='cas_login'),
     url(r'^logout/$', cas.views.logout, name='cas_logout'),
     url(r'^user-logout$', views.logout, name='user_logout'),
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name="index"),
     url(r'^get/(?P<last_updated>\d+)/(?P<start_date>\d+)/(?P<end_date>\d+)$', views.events_json, name='events-json'),
