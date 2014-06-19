095d98ed7969808cc123f7d390e25f8b0006cb57
naphatkrit <naphat.krit@gmail.com>
2014-06-18 22:48:06 -0700

    Added sample typescript codes

---
+++ nice/static/ts/sampleclient.js
@@ -0,0 +1,5 @@
+/// <reference path="typings/tsd.d.ts" />
+var $ = require('jquery');
+var ui = require('UI');
+
+var testView = new ui.View($('<div>'));
---
+++ nice/static/ts/sampleclient.ts
@@ -0,0 +1,5 @@
+/// <reference path="typings/tsd.d.ts" />
+import $ = require('jquery');
+import ui = require('UI');
+
+var testView: ui.View = new ui.View($('<div>'));
