52798ec15d861fe2639c72a351f47f22ac990385
naphatkrit <naphat.krit@gmail.com>
2014-06-22 10:35:17 -0700

    Tested more views in unit test

--- nice/static/ts/unittests/main.js
+++ nice/static/ts/unittests/main.js
@@ -1,10 +1,18 @@
-define(["require", "exports", 'jquery', '../library/CoreUI/View', '../library/Testers/ViewTester'], function(require, exports, $, View, ViewTester) {
+define(["require", "exports", 'jquery', '../library/CoreUI/FocusableView', '../library/PopUp/PopUpView', '../library/CoreUI/View', '../library/Testers/ViewTester'], function(require, exports, $, FocusableView, PopUpView, View, ViewTester) {
     $(function () {
         try  {
             println('Starting unit tests');
-            println('Testing View');
 
+            println('Testing View');
             var viewTester = new ViewTester('View', new View($('<div>')));
+            viewTester.run();
+
+            println('Testing FocusableView');
+            viewTester = new ViewTester('FocusableView', new FocusableView($('<div>')));
+            viewTester.run();
+
+            println('Testing PopUpView');
+            viewTester = new ViewTester('PopUpView', new PopUpView($('<div>')));
             viewTester.run();
 
             testsSucceeded();
--- nice/static/ts/unittests/main.ts
+++ nice/static/ts/unittests/main.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 import $ = require('jquery');
+import FocusableView = require('../library/CoreUI/FocusableView');
+import PopUpView = require('../library/PopUp/PopUpView');
 import View = require('../library/CoreUI/View');
 import ViewTester = require('../library/Testers/ViewTester');
 $(()=>
@@ -7,9 +9,17 @@ $(()=>
             try
             {
                 println('Starting unit tests');
-                println('Testing View');
 
+                println('Testing View');
                 var viewTester = new ViewTester<View>('View', new View($('<div>')));
+                viewTester.run();
+
+                println('Testing FocusableView');
+                viewTester = new ViewTester<FocusableView>('FocusableView', new FocusableView($('<div>')));
+                viewTester.run();
+
+                println('Testing PopUpView');
+                viewTester = new ViewTester<PopUpView>('PopUpView', new PopUpView($('<div>')));
                 viewTester.run();
 
                 testsSucceeded();
