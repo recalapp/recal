02c668ad0a520af4647d6b361c33b925ac5bf45a
Dyland Xue <dylandxue2012@gmail.com>
2014-11-04 09:48:16 -0500

    used service binding for queueCtrl

--- course_selection/static/js/controllers/QueueCtrl.js
+++ course_selection/static/js/controllers/QueueCtrl.js
@@ -3,20 +3,11 @@ define(["require", "exports"], function(require, exports) {
 
     var QueueCtrl = (function () {
         function QueueCtrl($scope, testSharingService) {
-            var _this = this;
             this.$scope = $scope;
             this.testSharingService = testSharingService;
             this.$scope.vm = this;
             this.$scope.data = testSharingService.getData();
-            this.$scope.$watch(function () {
-                return _this.$scope.data.enrolledCourses;
-            }, function (newCourses, oldCourses) {
-                return _this.updateEnrolledCourses(newCourses, oldCourses);
-            }, true);
         }
-        QueueCtrl.prototype.updateEnrolledCourses = function (newCourses, oldCourses) {
-            this.$scope.enrolledCourses = newCourses;
-        };
         QueueCtrl.$inject = [
             '$scope',
             'TestSharingService'
--- course_selection/static/js/controllers/QueueCtrl.ts
+++ course_selection/static/js/controllers/QueueCtrl.ts
@@ -15,18 +15,6 @@ class QueueCtrl {
     {
         this.$scope.vm = this;
         this.$scope.data = testSharingService.getData();
-        this.$scope.$watch(
-                () => { 
-                    return this.$scope.data.enrolledCourses; 
-                },
-                (newCourses, oldCourses) => { 
-                    return this.updateEnrolledCourses(newCourses, oldCourses); 
-                },
-                true);
-    }
-
-    public updateEnrolledCourses(newCourses, oldCourses) {
-        this.$scope.enrolledCourses = newCourses;
     }
 }
 
