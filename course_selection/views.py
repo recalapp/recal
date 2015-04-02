86aadcfed5dd1f1c7cf5313332007ba2f9d23cab
Dyland Xue <dylandxue2012@gmail.com>
2015-04-02 12:10:36 -0400

    disabled caching, added registar links to the info tags

--- course_selection/views.py
+++ course_selection/views.py
@@ -97,8 +97,9 @@ def get_courses_by_term_code(term_code):
 
 @login_required
 @require_GET
-@cache_page_with_prefix(60 * 5, lambda request: hashlib.md5(request.GET.get('semester__term_code', '')).hexdigest())
+# @cache_page_with_prefix(60 * 5, lambda request: hashlib.md5(request.GET.get('semester__term_code', '')).hexdigest())
 def get_courses_json(request):
+    #TODO disabled cache for now
     """
     Returns list of courses for a semester
     Cached for 5 minutes by ?semester__term_code
