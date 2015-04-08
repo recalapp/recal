267fc5ed3ca84d3a250e65359f70d5a948ad68fc
Maxim Zaslavsky <maxim@maximzaslavsky.com>
2015-04-08 13:59:35 -0400

    fixed #291

--- course_selection/urls.py
+++ course_selection/urls.py
@@ -23,6 +23,7 @@ v1_api.register(CourseListingResource())
 urlpatterns = patterns(
     "",
     url(r'^$', views.index, name="course_selection"),
+    url(r'^checks/continuity$', views.continuity_check, name="checks_continuity"),
     url(r'^course_evaluations/(?P<semester_id>\d+)/(?P<course_id>\d+)$', views.course_evaluations, name="course_evaluations"),
     url(r'^api/', include(v1_api.urls)),
     url(r'^api/static/courses/(?P<term_code>\d+)$', views.get_courses_json, name='get-courses-json'),
