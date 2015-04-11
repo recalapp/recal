3e4e358ff560f6c175b16c24c1bc163f261497bc
naphatkrit <naphat.krit@gmail.com>
2015-04-11 08:51:50 -0400

    course counter

--- course_selection/scrape_all.py
+++ course_selection/scrape_all.py
@@ -1,14 +1,18 @@
 from scrape_parse import scrape_parse_semester
 from scrape_validate import validate_course
-from scrape_import import scrape_import_course
+from scrape_import import scrape_import_course, ScrapeCounter
 
 def get_all_courses():
     # we can generate these given settings.CURR_TERM
     term_codes = [1162]
     for term_code in term_codes:
         try:
+            print "Scraping for semester " + str(term_code)
             courses = scrape_parse_semester(term_code)
             [validate_course(x) for x in courses] # just a sanity check in case we ever modify scrape_parse
-            [scrape_import_course(x) for x in courses]
+            scrapeCounter = ScrapeCounter()
+            [scrape_import_course(x, scrapeCounter) for x in courses]
+            print str(scrapeCounter)
+            print "----------------------------------"
         except Exception as e:
             print e
--- course_selection/scrape_import.py
+++ course_selection/scrape_import.py
@@ -1,4 +1,28 @@
-def scrape_import_course(course):
+class ScrapeCounter:
+    def __init__(self):
+        self.totalCoursesCount = 0
+        self.createdCoursesCount = 0
+        self.totalSectionsCount = 0
+        self.createdSectionsCount = 0
+        self.totalMeetingsCount = 0
+        self.createdMeetingsCount = 0
+        self.totalProfessorsCount = 0
+        self.createdProfessorsCount = 0
+        self.totalListingsCount = 0
+        self.createdListingsCount = 0
+    def __str__(self):
+        return str(self.createdCoursesCount) + " new courses\n" + \
+            str(self.totalCoursesCount) + " total courses\n" + \
+            str(self.createdSectionsCount) + " new sections\n" + \
+            str(self.totalSectionsCount) + " total sections\n" + \
+            str(self.createdMeetingsCount) + " new meetings\n" + \
+            str(self.totalMeetingsCount) + " total meetings\n" + \
+            str(self.createdProfessorsCount) + " new professors\n" + \
+            str(self.totalProfessorsCount) + " total professors\n" + \
+            str(self.createdListingsCount) + " new listings\n" + \
+            str(self.totalListingsCount) + " total listings"
+
+def scrape_import_course(course, counter=ScrapeCounter()):
     from models import Course
     def import_section(section, course_object):
         from models import Section, Meeting
@@ -10,6 +34,9 @@ def scrape_import_course(course):
                 days = meeting['days'],
                 location = meeting['location']
             )
+            if created:
+                counter.createdMeetingsCount += 1
+            counter.totalMeetingsCount += 1
             return meeting_object
         section_object, created = Section.objects.get_or_create(
             course=course_object,
@@ -21,6 +48,9 @@ def scrape_import_course(course):
         Meeting.objects.filter(section=section_object).delete()
         [import_meeting(x, course_object, section_object) for x in section['meetings']]
         section_object.save()
+        if created:
+            counter.createdSectionsCount += 1
+        counter.totalSectionsCount += 1
         return section_object
 
     def import_professor(prof, course_object):
@@ -30,6 +60,9 @@ def scrape_import_course(course):
         )
         course_object.professors.add(prof_object)
         prof_object.save()
+        if created:
+            counter.createdProfessorsCount += 1
+        counter.totalProfessorsCount += 1
         return course_object
 
     def import_listing(listing, course_object):
@@ -40,6 +73,9 @@ def scrape_import_course(course):
             number=listing['code'],
             is_primary=listing['is_primary']
         )
+        if created:
+            counter.createdListingsCount += 1
+        counter.totalListingsCount += 1
         return listing_object
 
     def import_semester(semester):
@@ -61,3 +97,7 @@ def scrape_import_course(course):
     [import_professor(x, course_object) for x in course['professors']]
     [import_listing(x, course_object) for x in course['course_listings']]
     course_object.save()
+    if created:
+        counter.createdCoursesCount += 1
+    counter.totalCoursesCount += 1
+    return counter
